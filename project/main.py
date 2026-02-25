4fe993b1674a9acd0ac0dcf08e6655325c2508af
yla0123 <yla12302@gmail.com>
2026-02-25 13:15:03 -0500

    Update main.py

--- project/main.py
+++ project/main.py
@@ -1,1 +1,20 @@
+import cv2
 
+ESP32_IP = ""   
+url = f"http://{ESP32_IP}:81/stream"
+
+cap = cv2.VideoCapture(url)
+
+while True:
+    ret, frame = cap.read()  # ret is a Boolean (True when captured), frame is numPy array
+    if not ret:
+        print("Failed to grab frame")
+        break
+
+    cv2.imshow("ESP32-CAM", frame) 
+
+    if cv2.waitKey(1) & 0xFF == ord('q'): # press q to quit
+        break
+
+cap.release()
+cv2.destroyAllWindows()
