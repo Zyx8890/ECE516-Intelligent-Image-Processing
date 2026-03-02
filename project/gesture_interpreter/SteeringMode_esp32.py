c173ffa3f3dea6f31409d970a5554c57b74e8229
yla0123 <yla12302@gmail.com>
2026-03-02 12:26:30 -0500

    Create SteeringMode_esp32.py

---
+++ project/gesture_interpreter/SteeringMode_esp32.py
@@ -0,0 +1,106 @@
+import cv2
+import numpy as np
+import mediapipe as mp
+
+# REPLACE with your ESP32-CAM IP address
+url = "http://172.20.10.2:81/stream"
+
+def main():
+    mp_hands = mp.solutions.hands #pre-trained DL model for hand recognition
+    hands = mp_hands.Hands(
+        static_image_mode=False, 
+        max_num_hands=2,               # Detect both hands
+        min_detection_confidence=0.7,  # Set confidence to 70%
+        min_tracking_confidence=0.7
+    )
+    # Create a video capture object
+    cap = cv2.VideoCapture(url)
+
+    print("Connecting to stream...")
+
+    while cap.isOpened():
+        success, frame = cap.read()
+        if not success:
+            print("Failed to grab frame")
+            break
+
+        # 1. Flip and convert color for MediaPipe
+        frame = cv2.flip(frame, 1)
+        img_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = hands.process(img_rgb) # hand recognition
+
+        # 2. If hands are detected
+        if results.multi_hand_landmarks: 
+            h, w, _ = frame.shape  # Get frame dimensions
+
+            for idx, hand_landmarks in enumerate(results.multi_hand_landmarks):
+                # hand_landmarks contains 21 joint points, each has a (x,y,z) coordinate
+                label = results.multi_handedness[idx].classification[0].label
+
+                # find the min/max x and y to draw a box
+                x_min, y_min = w, h
+                x_max, y_max = 0, 0
+
+                for lm in hand_landmarks.landmark:
+                    # convert normalized coordinates (0-1) to pixel coordinates
+                    px, py = int(lm.x * w), int(lm.y * h)
+                    
+                    if px < x_min: x_min = px
+                    if py < y_min: y_min = py
+                    if px > x_max: x_max = px
+                    if py > y_max: y_max = py
+
+                # 3. Draw the Green Box and add labels
+                # add a little padding (20 pixels) so the box isn't too tight
+                cv2.rectangle(frame, (x_min - 20, y_min - 20), (x_max + 20, y_max + 20), (0, 255, 0), 2)
+                
+                # 4. Speed control with left hand
+                if label == "Left":
+                    fingerUP = []
+                    fingertips = [20,16,12,8] #index of fingertips
+                    knuckles = [18,14,10,6]   #index of knuckles
+
+                    # fingers
+                    for i in range(4):
+                        if hand_landmarks.landmark[fingertips[i]].y < hand_landmarks.landmark[knuckles[i]].y:
+                            fingerUP.append(1)
+                        else:
+                            fingerUP.append(0)
+                    
+                    # thumb
+                    if hand_landmarks.landmark[4].x < hand_landmarks.landmark[2].x:
+                        fingerUP.append(1)
+                    else:
+                        fingerUP.append(0)
+
+                    cv2.putText(frame, str(sum(fingerUP)), (x_max, y_min - 20),cv2.FONT_HERSHEY_DUPLEX,1.5,(255, 255, 255),2)
+                    cv2.putText(frame, "L", (x_min - 20, y_min - 20),cv2.FONT_HERSHEY_DUPLEX,1.5,(255, 255, 255),2)
+
+                # 5. navigate with right hand
+                if label == "Right":
+                    wrist_x = hand_landmarks.landmark[9].x
+                    middle_x = hand_landmarks.landmark[0].x
+
+                    # calculate the x difference of middle finger and wrist
+                    diff = hand_landmarks.landmark[0].x - hand_landmarks.landmark[9].x
+                    if diff > 0.05: 
+                        navigation = "right"
+                    elif diff < -0.05: 
+                        navigation = "left"
+                    else: 
+                        navigation = "straight"
+
+                    cv2.putText(frame, navigation, (x_max, y_min - 20),cv2.FONT_HERSHEY_DUPLEX,1.5,(255, 255, 255),2)
+                    cv2.putText(frame, "R", (x_min - 20, y_min - 20),cv2.FONT_HERSHEY_DUPLEX,1.5,(255, 255, 255),2)
+
+        cv2.imshow("ESP32-CAM Live Stream", frame)
+        #cv2.imshow("ESP32-CAM Live Stream", cv2.flip(frame, 1))
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # Cleanup
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
